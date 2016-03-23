391e0b76512d5b38a4de7b6ff0b221e4314e316a
abbasaniefa <abbasvlb@gmail.com>
2016-03-23 09:24:32 +0530

    Initial Commit

---
+++ MeiMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeiMe
+//
+//  Created by Abbas Aniefa on 23/03/16.
+//  Copyright © 2016 Abbas Aniefa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
